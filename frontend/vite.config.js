d62b1fea75cdf83cc96a010e397664e321f554ca
Otech <otengebenezer326@gmail.com>
2026-03-12 15:22:51 -0700

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    open: true
+    open: true,
+    historyApiFallback: true, // This ensures all routes fallback to index.html
   },
-  
+  preview: {
+    port: 3000,
+    open: true,
+    historyApiFallback: true, // Also for preview/production build
+  }
 })
